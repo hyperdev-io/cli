6afdee9f80d7c06261fdd9af18afd879ac713574
Gantcho Kojuharov <gantcho@greyarch.net>
2018-01-24 13:41:02 +0100

    use async/await for apps commands

--- src/commands/app.js
+++ src/commands/app.js
@@ -4,47 +4,37 @@ const Table = require("cli-table2");
 module.exports = ({ client, tableStyle }) => (vorpal, options) => {
   vorpal
     .command("app ls", "List applications.")
-    .action(function(args, callback) {
-      client.apps
-        .list()
-        .then(apps => {
-          const table = new Table(
-            Object.assign(
-              { head: ["ID", "NAME", "VERSION", "TAGS"] },
-              tableStyle
-            )
-          );
-          for (app of apps) {
-            table.push([
-              app.id,
-              app.name,
-              app.version,
-              app.tags ? app.tags.join(",") : ""
-            ]);
-          }
-          this.log(table.toString());
-          callback();
-        })
-        .catch(error => {
-          this.log(error.graphQLErrors[0].message);
-          callback();
-        });
+    .action(async function(args, callback) {
+      try {
+        const apps = await client.apps.list();
+        const table = new Table(
+          Object.assign({ head: ["ID", "NAME", "VERSION", "TAGS"] }, tableStyle)
+        );
+        for (app of apps) {
+          table.push([
+            app.id,
+            app.name,
+            app.version,
+            app.tags ? app.tags.join(",") : ""
+          ]);
+        }
+        this.log(table.toString());
+        callback && callback();
+      } catch (error) {
+        console.error(error.message);
+      }
     });
   vorpal
     .command("app rm <name:version>", "Remove an application.")
-    .action(function(args, callback) {
-      const appName = args["name:version"].split(":")[0];
-      const appVersion = args["name:version"].split(":")[1];
-      client.apps
-        .remove(appName, appVersion)
-        .then(app => {
-          this.log(app);
-          callback();
-        })
-        .catch(error => {
-          this.log(error.graphQLErrors[0].message);
-          callback();
-        });
+    .action(async function(args, callback) {
+      const [appName, appVersion] = args["name:version"].split(":");
+      try {
+        const app = await client.apps.remove(appName, appVersion);
+        this.log(app);
+        callback && callback();
+      } catch (error) {
+        console.error(error.message);
+      }
     });
   vorpal
     .command(
@@ -53,26 +43,19 @@ module.exports = ({ client, tableStyle }) => (vorpal, options) => {
     )
     // .option("-d, --docker-compose <path>", "Path to the Docker Compose file.")
     // .option("-b, --bigboat-compose <path>", "Path to the BigBoat Compose file.")
-    .action(function(args, callback) {
+    .action(async function(args, callback) {
       const [appName, appVersion] = args["name:version"].split(":");
       const dockerComposeFile = args["docker-compose-file"];
       const bigboatComposeFile = args["bigboat-compose-file"];
       try {
-        client.apps
-          .createOrUpdate(
-            appName,
-            appVersion,
-            fs.readFileSync(dockerComposeFile, { encoding: "utf8" }),
-            fs.readFileSync(bigboatComposeFile, { encoding: "utf8" })
-          )
-          .then(app => {
-            this.log(app.id);
-            callback();
-          })
-          .catch(error => {
-            this.log(error.graphQLErrors[0].message);
-            callback();
-          });
+        const app = await client.apps.createOrUpdate(
+          appName,
+          appVersion,
+          fs.readFileSync(dockerComposeFile, { encoding: "utf8" }),
+          fs.readFileSync(bigboatComposeFile, { encoding: "utf8" })
+        );
+        this.log(app.id);
+        callback && callback();
       } catch (error) {
         console.error(error.message);
       }
